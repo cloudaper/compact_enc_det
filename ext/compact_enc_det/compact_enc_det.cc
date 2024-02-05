ce6afead1568f25fcbb9d2d83a4e70de85b85d50
Kryštof Korb <krystof@korb.cz>
2024-02-05 19:12:34 +0100

    Refactor to return class instead of array

--- ext/compact_enc_det/compact_enc_det.cc
+++ ext/compact_enc_det/compact_enc_det.cc
@@ -1,7 +1,36 @@
 #include <ruby.h>
 #include "compact_enc_det/compact_enc_det/compact_enc_det.h"
 
-// CompactEncDet::DetectEncoding wrapper for Ruby
+// Define custom Ruby class CompactEncDet::DetectEncodingResult
+// for the result of CompactEncDet.detect_encoding
+void Init_detect_encoding_result(VALUE rb_mCompactEncDet);
+static VALUE rb_cDetectEncodingResult;
+
+static VALUE detect_encoding_result_encoding(VALUE self)
+{
+  return rb_iv_get(self, "@encoding");
+}
+
+static VALUE detect_encoding_result_bytes_consumed(VALUE self)
+{
+  return rb_iv_get(self, "@bytes_consumed");
+}
+
+static VALUE detect_encoding_result_is_reliable(VALUE self)
+{
+  return rb_iv_get(self, "@is_reliable");
+}
+
+void Init_detect_encoding_result(VALUE rb_mCompactEncDet)
+{
+  rb_cDetectEncodingResult = rb_define_class_under(rb_mCompactEncDet, "DetectEncodingResult", rb_cObject);
+  rb_define_method(rb_cDetectEncodingResult, "encoding", RUBY_METHOD_FUNC(detect_encoding_result_encoding), 0);
+  rb_define_method(rb_cDetectEncodingResult, "bytes_consumed", RUBY_METHOD_FUNC(detect_encoding_result_bytes_consumed), 0);
+  rb_define_method(rb_cDetectEncodingResult, "is_reliable?", RUBY_METHOD_FUNC(detect_encoding_result_is_reliable), 0);
+}
+
+// Ruby wrapper CompactEncDet.detect_encoding
+// for the CompactEncDet::DetectEncoding C++ function
 static VALUE detect_encoding(int argc, VALUE *argv, VALUE self)
 {
   VALUE ruby_text,
@@ -47,17 +76,17 @@ static VALUE detect_encoding(int argc, VALUE *argv, VALUE self)
       &bytes_consumed,
       &is_reliable);
 
-  // Return the encoding enum integer, bytes consumed integer
-  // and is reliable boolean in an array
-  VALUE result = rb_ary_new();
-  rb_ary_push(result, rb_int_new(encoding));
-  rb_ary_push(result, rb_int_new(bytes_consumed));
-  rb_ary_push(result, is_reliable ? Qtrue : Qfalse);
+  // Return the detected encoding as a Ruby class
+  VALUE result = rb_class_new_instance(0, NULL, rb_cDetectEncodingResult);
+  rb_iv_set(result, "@encoding", rb_int_new(encoding));
+  rb_iv_set(result, "@bytes_consumed", rb_int_new(bytes_consumed));
+  rb_iv_set(result, "@is_reliable", is_reliable ? Qtrue : Qfalse);
   return result;
 }
 
 extern "C" void Init_compact_enc_det()
 {
-  VALUE module = rb_define_module("CompactEncDet");
-  rb_define_module_function(module, "detect_encoding", RUBY_METHOD_FUNC(detect_encoding), -1);
+  VALUE rb_mCompactEncDet = rb_define_module("CompactEncDet");
+  Init_detect_encoding_result(rb_mCompactEncDet);
+  rb_define_module_function(rb_mCompactEncDet, "detect_encoding", RUBY_METHOD_FUNC(detect_encoding), -1);
 }
--- test/compact_enc_det_test.rb
+++ test/compact_enc_det_test.rb
@@ -5,12 +5,9 @@ class CompactEncDetTest < Minitest::Test
   def test_detect_encoding_known_english
     text = File.read("test/fixtures/utf-8.txt")
     result = CompactEncDet.detect_encoding(text, text.bytesize)
-    encoding_detected = result[0]
-    bytes_consumed = result[1]
-    is_reliable = result[2]
 
-    assert_equal 22, encoding_detected
-    assert_operator 0, :<, bytes_consumed
-    assert_equal true, is_reliable
+    assert_equal 22, result.encoding
+    assert_operator 0, :<, result.bytes_consumed
+    assert_equal true, result.is_reliable?
   end
 end
